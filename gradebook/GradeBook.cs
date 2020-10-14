dd1dc556750961a94115664669996c09bcae1ab9
JHussle <smithmjoey@outlook.com>
2020-10-14 17:31:18 -0500

    Delete GradeBook.cs

--- gradebook/GradeBook.cs
+++
@@ -1,9 +0,0 @@
-namespace Joey.Documents.GitHub.C_Language_Portfolio.gradebook
-{
-    public class GradeBook
-    {
-        /* This will be a new gradebook project. The person of this book is to...
-        House student grades (Homework, Quiz scores, Exam scores)
-        
-    }
-}
