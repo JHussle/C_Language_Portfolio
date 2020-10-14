49e7b3dc3dd505850f2dc8ae98622cd41a58e5bd
JHussle <smithmjoey@outlook.com>
2020-10-14 17:30:37 -0500

    Update GradeBook.cs

--- gradebook/GradeBook.cs
+++ gradebook/GradeBook.cs
@@ -5,10 +5,5 @@ namespace Joey.Documents.GitHub.C_Language_Portfolio.gradebook
         /* This will be a new gradebook project. The person of this book is to...
         House student grades (Homework, Quiz scores, Exam scores)
         
-        This book should also store student names, student ID, student contact information
-        
-        this gradebook should also store student class schedule as well.
-        
-        starting the project small then expanding into a database.*/
     }
 }
