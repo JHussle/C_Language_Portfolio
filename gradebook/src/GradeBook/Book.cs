b22af5a52072525b8caccce168079cb602e75a5b
JHussle <smithmjoey@outlook.com>
2020-10-15 12:59:28 -0500

    updates

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -1,7 +1,12 @@
+using System.Collections.Generic;
+
 namespace GradeBook
 {
     public class Book
     {
-        
+        public void AddGrade(double grade) {
+            grades.Add(grade);
+        }
+        List<double> grades;
     }
 }
